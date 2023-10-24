1a0636d87a70870d4b8169f9c8cc5a6721df97a4
joshuanel10 <12660812+joshuanel10@users.noreply.github.com>
2023-10-24 15:39:16 +0200

    Update rockpaperscissors.js
    - Added game logic

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,8 +1,49 @@
+let person = prompt("Please choose rock, paper or scissors");
+let computer = getComputerChoice();
+
+
 function getComputerChoice() {
    const choices = ["rock","paper","scissors"]
    var choice = choices[Math.floor(Math.random()*choices.length)]
    return choice 
 }
 
+function playGame(player,computer){
+    if (player == "rock"){
+        if (computer == "rock"){
+            return "It's a tie! Both players chose rock"
+        } else if (computer == "paper") {
+            return "You lose! Paper beats rock"
+        } else {
+            return "You win! Rock beats scissors"
+        }
+    }
 
-console.log(getComputerChoice())
+    if (player == "paper"){
+        if (computer == "paper"){
+            return "It's a tie! Both players chose Paper"
+        } else if (computer == "scissors") {
+            return "You lose! Scissors beats Paper"
+        } else {
+            return "You win! Paper beats Rock"
+        }
+    }
+
+    if (player == "scissors"){
+        if (computer == "scissors"){
+            return "It's a tie! Both players chose Scissors"
+        } else if (computer == "paper") {
+            return "You lose! Rock beats Scissors"
+        } else {
+            return "You win! Scissors beats Paper"
+        }
+    }
+}
+
+
+
+
+
+console.log(computer + " : Computers Choice");
+console.log(person + " : Player Choice");
+console.log(playGame(person,computer));
